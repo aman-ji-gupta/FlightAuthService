8504fcbc0f4ecd10dfa7c8cf5c75e1d59c9bc977
aman-ji-gupta <gamansantosh811@gmail.com>
2023-04-12 18:36:52 +0530

    added User Service

---
+++ src/services/user-service.js
@@ -0,0 +1,30 @@
+const UserRepository = require("../repository/user-repository");
+
+class UserService{
+
+    constructor(){
+        this.userRepository = new UserRepository();
+    }
+
+    async create(data){
+       try {
+        const user = await this.userRepository.create(data);
+        return user;
+       } catch (error) {
+        console.log("Something went wrong at the service layer..!");
+        throw error;
+        }
+    }
+
+    async destroy(id){
+        try {
+         await this.userRepository.destroy(id);
+         return true;
+        } catch (error) {
+         console.log("Something went wrong at the service layer..!");
+         throw error;
+        }
+     }
+}
+
+module.exports = UserService;
